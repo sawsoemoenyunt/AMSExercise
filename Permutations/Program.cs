7a108f35147b1e03f9e348b2b824d1dad2b33aa5
sawsoemoenyunt <sawsoemoenyunt@gmail.com>
2019-09-11 21:34:28 +1000

    update permutations wed 11 sep 2019

--- Permutations/Program.cs
+++ Permutations/Program.cs
@@ -1,62 +1,56 @@
 ﻿using System;
 
-namespace Permutations
+public class Permutations
 {
-    public class Permutations
+    // Helper method for outputting an array.
+    private static void PrintArray(string[] array)
     {
-        // Helper method for outputting an array.
-        private static void PrintArray(string[] array)
+        foreach (string element in array)
         {
-            foreach (string element in array)
-            {
-                Console.Write($"{element} ");
-            }
-            Console.WriteLine();
+            Console.Write($"{element} ");
         }
+        Console.WriteLine();
+    }
 
-        // Helper method for invoking Generate.
-        private static void Generate(int index, string[] arr)
+    private static void Generate(int k, string[] a)
+    {
+        if (k == 1)
         {
-            if (index == 1)
+            PrintArray(a);
+        }
+        else
+        {
+            // Generate permutations with kth unaltered
+            // Initially k == length(A)
+            Generate(k - 1, a);
+
+            //// Generate permutations for kth swapped with each k-1 initial
+            for (int i = 0; i < k - 1; i++)
             {
-                PrintArray(arr);
-            }
-            else
-            {
-                // Generate permutations with kth unaltered
-                // Initially k == length(A)
-                
-                if (index == arr.Length)
+                string temp1, temp2;
+
+                if (k % 2 == 0)
                 {
-                    Generate(index - 1, arr);
-
-                    // Generate permutations for kth swapped with each k-1 initial
-                    for (int i = 0; i < index - 1; i++)
-                    {
-                        // Swap choice dependent on parity of k (even or odd)
-                        if (index % 2 == 0)
-                        {
-                            //even
-                            arr[i] = arr[index - 1];
-                            arr[index - 1] = arr[i];
-                        }
-                        else
-                        {
-                            //odd
-                            string temp = arr[0];
-                            arr[0] = arr[index - 1];
-                            arr[index - 1] = temp;
-                        }
-                        Generate(index - 1, arr);
-                    }
+                    temp1 = a[i];
+                    temp2 = a[k - 1];
+                    a[i] = temp2;
+                    a[k - 1] = temp1;
                 }
+                else
+                {
+                    temp1 = a[0];
+                    temp2 = a[k - 1];
+                    a[0] = temp2;
+                    a[k - 1] = temp1;
+                }
+                Generate(k - 1, a);
             }
-        }
-
-        public static void Main(string[] args)
-        {
-            string[] arr = { "qaz", "wsx", "edc" };
-            Generate(2, arr);
         }
     }
+
+    public static void Main(string[] args)
+    {
+        Generate(args.Length, args);
+    }
 }
+
